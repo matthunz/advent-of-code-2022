394eb0abcc92652c53f126be8481fb366a34b890
Matt Hunzinger <matthunz2@gmail.com>
2023-07-11 07:36:26 -0400

    Clean up part 2

--- src/Part2.hs
+++ src/Part2.hs
@@ -8,12 +8,6 @@ moveScore move = case move of
   Paper -> 2
   Scissors -> 3
 
-parseTheirMove :: String -> Move
-parseTheirMove line = case line of
-  "A" -> Rock
-  "B" -> Paper
-  _ -> Scissors
-
 data Turn = Turn
   { yourMove :: Move,
     theirMove :: Move
@@ -21,50 +15,52 @@ data Turn = Turn
 
 data Outcome = Win | Loss | Tie
 
-outcome :: Turn -> Outcome
-outcome turn = case turn of
-  Turn Rock Paper -> Loss
-  Turn Rock Scissors -> Win
-  Turn Paper Rock -> Win
-  Turn Paper Scissors -> Loss
-  Turn Scissors Rock -> Loss
-  Turn Scissors Paper -> Win
-  _ -> Tie
-
 score :: Turn -> Int
 score turn =
-  mvScore + case outcome turn of
+  mvScore + case outcome of
     Win -> 6
     Tie -> 3
     Loss -> 0
   where
     mvScore = moveScore $ yourMove turn
+    outcome = case turn of
+      Turn Rock Paper -> Loss
+      Turn Rock Scissors -> Win
+      Turn Paper Rock -> Win
+      Turn Paper Scissors -> Loss
+      Turn Scissors Rock -> Loss
+      Turn Scissors Paper -> Win
+      _ -> Tie
 
-parseLine :: String -> Turn
-parseLine line = Turn you them
+solve :: (Move -> String -> Move) -> String -> Int
+solve makeMove input = sum $ map (score . p) (lines input)
   where
-    w = words line
-    them = parseTheirMove $ head w
-    you = case w !! 1 of
+    p line = Turn you them
+      where
+        w = words line
+        them = case head w of
+          "A" -> Rock
+          "B" -> Paper
+          _ -> Scissors
+        you = makeMove them (w !! 1)
+
+solve2a :: String -> Int
+solve2a = solve makeMove
+  where
+    makeMove _ s = case s of
       "X" -> Rock
       "Y" -> Paper
       _ -> Scissors
 
-solve2a :: String -> Int
-solve2a input = sum $ map (score . parseLine) (lines input)
-
 solve2b :: String -> Int
-solve2b input = sum $ map (score . turnB . parseLineB) (lines input)
+solve2b = solve makeMove
   where
-    parseLineB line = (you, them)
-      where
-        w = words line
-        them = parseTheirMove $ head w
-        you = case w !! 1 of
-          "X" -> Loss
-          "Y" -> Tie
-          _ -> Win
-    turnB (out, move) = Turn yourMove move
+    makeMove them s = getMove (getOutcome s) them
+    getOutcome s = case s of
+      "X" -> Loss
+      "Y" -> Tie
+      _ -> Win
+    getMove out move = yourMove
       where
         yourMove = case out of
           Win -> case move of
